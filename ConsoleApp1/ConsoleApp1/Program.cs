f88eca0953ce97f717a6491e2d6a4e98e2b1d865
ipz235_sag <ipz235_sag@student.ztu.edu.ua>
2025-01-25 16:36:47 +0200

    Створено консольний додаток, README та .gitignore

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введiть перше число: ");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введiть друге число: ");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            double sum = number1 + number2;
+
+            Console.WriteLine($"Сума чисел {number1} i {number2} дорiвнює {sum}");
+        }
+    }
+}
